dd976fa88e4bfec9d525a097f4cb70db3775064a
74dydgur <74dydgur@naver.com>
2022-12-29 17:18:47 +0900

    1stwebapp

---
+++ simplywebserver.py
@@ -0,0 +1,20 @@
+#필요한 모듈을 단계적으로 import
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+# 테스트 기능
+@app.route('/hello')
+def hello_Flask():
+    return 'Hello, Flask'
+
+@app.route('/hi')
+def hi_Flask():
+    return 'Hi, Flask'
+
+if __name__ == '__main__':
+    app.run(
+    host="0.0.0.0",
+    port=7777,
+    debug=True)
+ 
